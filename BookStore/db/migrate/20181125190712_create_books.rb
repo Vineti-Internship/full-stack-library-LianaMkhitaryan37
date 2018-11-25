913817039944f4c0137aa3b3067332722bd3e92e
Liana Mkhitaryan <liana.mkhitaryan37@gmail.com>
2018-11-25 23:44:28 +0400

    Created Associations between Author and Books

--- BookStore/db/migrate/20181125190712_create_books.rb
+++ BookStore/db/migrate/20181125190712_create_books.rb
@@ -1,7 +1,7 @@
 class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
-      t.reference :user
+      t.references :user
       t.string :title
       t.text :description
       t.string :type
