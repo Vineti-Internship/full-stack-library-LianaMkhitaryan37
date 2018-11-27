257997da2e308861ee41bfa0a43ca8c801ceb40a
Liana Mkhitaryan <liana.mkhitaryan37@gmail.com>
2018-11-27 19:27:04 +0400

    Adding namespace(V1) and scope to api and also add devise gem

--- BookStore/config/routes.rb
+++ BookStore/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :books
-  resources :authors
+
+  scope 'api' do
+    namespace :v1 do
+      resources :books
+      resources :authors
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
